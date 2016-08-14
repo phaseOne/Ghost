3ec40a230f0363561bcdd388826aef5b81a391c1
Evan Bovie <evan@bovie.me>
2016-08-14 04:04:31 -0700

    title how I like it

--- core/server/data/meta/title.js
+++ core/server/data/meta/title.js
@@ -14,13 +14,19 @@ function getTitle(data, root) {
     if (data.meta_title) {
         title = data.meta_title;
     } else if (_.includes(context, 'home')) {
-        title = blog.title;
+        title = config.channels.index.title || blog.title;
+    } else if (_.includes(context, 'blog')) {
+        title = 'Blog';
+        if (pagination.page > 1) title += pageString;
+        title += ' · ' + blog.title;
     } else if (_.includes(context, 'author') && data.author) {
         title = data.author.name + pageString + ' - ' + blog.title;
     } else if (_.includes(context, 'tag') && data.tag) {
         title = data.tag.meta_title || data.tag.name + pageString + ' - ' + blog.title;
-    } else if ((_.includes(context, 'post') || _.includes(context, 'page')) && data.post) {
+    } else if (_.includes(context, 'post') && data.post) {
         title = data.post.meta_title || data.post.title;
+    } else if (_.includes(context, 'page') && data.post) {
+        title = (data.post.meta_title || data.post.title) + ' · ' + blog.title;
     } else {
         title = blog.title + pageString;
     }
